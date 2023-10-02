f47aa68062aed1fd08446d9cd7dbcdc1354ca572
CaptainKus <cpt.adriankus@gmail.com>
2023-10-02 17:59:19 +0300

    Hands-on folder added

---
+++ hands-on/flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello world from Flask!"
+
+@app.route("/second")
+def second():
+    return "This is the second page"
+
+@app.route("/third/subthird")
+def third():
+    return "This is the subpath of third path"
+
+@app.route("/forth/<string:id>")
+def forth(id):
+    return f'Id of this page is {id}'
+
+if __name__ == "__main__":
+    app.run(debug=True, port=2000)
